dde2b2a337bf2b0a3f7202facd82f533ae29d7d7
alexandre-assad <alexandre.assad@laplateforme.io>
2023-06-11 17:12:34 +0200

    [ADD] Create potential_values fuctions

--- src/generator/grid.py
+++ src/generator/grid.py
@@ -1,4 +1,4 @@
-
+from src.utils.numpy_manager import *
 
 class Grid:
     
@@ -13,15 +13,27 @@ class Grid:
     
     """
     Input : two int and a matrix, coordinates x and y of a case, with the matrix of the grid
+    Basic structure : Find the 3x3 box where the case is, and test all the available values left
     Outpout : a list, All the potential value the case could have in the box (3x3)
     """
     def potential_value_box(self,x,y):
-        pass
-    
+        list_values = []
+        box = [x//3,y//3]
+        for i in range(3):
+            for j in range(3):
+                list_values.append(self.g_matrix[box[0]*3+i][box[1]*3+j])
+        return exter(list_values,[1,2,3,4,5,6,7,8,9])
     
     """
     Input : two int and a matrix, coordinates x and y of a case, with the matrix of the grid
+    Basic code : Test all the available values for each axis
     Outpout : a list, All the potential value the case could have in the two axis (y-axis and x-axis)
     """
     def potential_value_lines(self,x,y):
-        pass
+        list_values_1 = []
+        list_values_2 = []
+        for i in range(9):
+            list_values_1.append(self.g_matrix[i][y])
+        for j in range(9):
+            list_values_2.append(self.g_matrix[x][j])
+        return exter(exter(list_values_1,[1,2,3,4,5,6,7,8,9]),list_values_2)
--- src/utils/numpy_manager.py
+++ src/utils/numpy_manager.py
@@ -1,4 +1,4 @@
-import numpy 
+import numpy as np
 
 
 
@@ -7,4 +7,16 @@ Input : two lists
 Output : One list, which is the intersection of the two input's lists
 """
 def inter(arr1:list,arr2:list) -> list:
-    return numpy.intersect1d(arr1,arr2)
+    return np.intersect1d(arr1,arr2)
+
+
+""" 
+Input : two lists
+Output : One list, which is the opposite of the intersection of the two input's lists
+"""
+def exter(arr1:list,arr2:list) -> list:
+    
+    dif1 = np.setdiff1d(arr1, arr2)
+    dif2 = np.setdiff1d(arr2, arr1)
+    temp3 = np.concatenate((dif1, dif2))
+    return temp3
